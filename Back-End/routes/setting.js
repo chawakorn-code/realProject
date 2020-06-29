9c284fa1e18b428e0183d6a7841025fdea32f0c0
Phatthanakorn Uraiwong <peatzahot@hotmail.com>
2020-06-29 15:48:52 +0700

    delete department

--- Back-End/routes/setting.js
+++ Back-End/routes/setting.js
@@ -24,6 +24,7 @@ db.connect((err) => {
     console.log('mysql connected.')
 })
 
+//add department
 router.post('/department', (req,res) => {
     var No = req.body.No
     var name = req.body.name
@@ -33,12 +34,37 @@ router.post('/department', (req,res) => {
             throw err
         }
     res.send(results)
-    console.log('001')
     })
 })
 
+//get all department
 router.get('/department', (req,res) => {
-    sql = 'SELECT * FROM `department`;'
+    sql = "SELECT * FROM `department`;"
+    db.query(sql, (err, results) => {
+        if(err){
+            throw err
+        }
+    res.send(results)
+    })
+})
+
+//delete department
+router.delete('/department', (req,res) => {
+    var id = req.body.id
+    sql = "DELETE FROM `department` WHERE `department`.`id` = " + id
+    db.query(sql, (err, results) => {
+        if(err){
+            throw err
+        }
+    res.send(results)
+    })
+})
+
+//add location
+router.post('/location', (req,res) => {
+    var No = req.body.No
+    var name = req.body.name
+    sql = "INSERT INTO `location` (`id`, `No`, `name`) VALUES (NULL, '"+ No +"', '" + name + "');"
     db.query(sql, (err, results) => {
         if(err){
             throw err
