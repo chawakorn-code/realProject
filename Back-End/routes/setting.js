6a76b8e4578472416b34668d0e6311945fa0317e
Phatthanakorn Uraiwong <peatzahot@hotmail.com>
2020-07-01 16:22:53 +0700

    get, add, update, delete user

--- Back-End/routes/setting.js
+++ Back-End/routes/setting.js
@@ -26,9 +26,10 @@ db.connect((err) => {
 
 //add department
 router.post('/department', (req,res) => {
+    var id = req.body.id
     var No = req.body.No
     var name = req.body.name
-    sql = "INSERT INTO `department` (`id`, `No`, `name`) VALUES (NULL, '"+ No +"', '" + name + "');"
+    sql = "INSERT INTO `department` (`id`, `No`, `name`) VALUES ('" + id + "', '"+ No +"', '" + name + "');"
     db.query(sql, (err, results) => {
         if(err){
             throw err
@@ -77,9 +78,10 @@ router.delete('/department', (req,res) => {
 
 //add location
 router.post('/location', (req,res) => {
+    var id = req.body.id
     var No = req.body.No
     var name = req.body.name
-    sql = "INSERT INTO `location` (`id`, `No`, `name`) VALUES (NULL, '"+ No +"', '" + name + "');"
+    sql = "INSERT INTO `location` (`id`, `No`, `name`) VALUES ('" + id + "', '"+ No +"', '" + name + "');"
     db.query(sql, (err, results) => {
         if(err){
             throw err
@@ -118,6 +120,68 @@ router.patch('/location/:id', (req,res) => {
     var No = req.body.No
     var name = req.body.name
     sql = "UPDATE `location` SET `id` = '" + newid + "', `No` = '" + No + "', `name` = '" + name + "' WHERE `location`.`id` = " + id + ";"
+    db.query(sql, (err, results) => {
+        if(err){
+            throw err
+        }
+    res.send(results)
+    })
+})
+
+//add user
+router.post('/user', (req,res) => {
+    var id = req.body.id
+    var username = req.body.username
+    var password = req.body.password
+    var name = req.body.name
+    var email = req.body.email
+    var role = req.body.role
+    var departmentID = req.body.departmentID
+    var status = req.body.status
+    sql = "INSERT INTO `user` (`id`, `username`, `password`, `name`, `email`, `role`, `departmentID`, `status`) VALUES ('" + id + "', '" + username + "', '" + password + "', '" + name + "', '" + email + "', '" + role + "', '" + departmentID + "', '" + status + "');"
+    db.query(sql, (err, results) => {
+        if(err){
+            throw err
+        }
+    res.send(results)
+    })
+})
+
+//get all user
+router.get('/user', (req,res) => {
+    sql = "SELECT * FROM `user`;"
+    db.query(sql, (err, results) => {
+        if(err){
+            throw err
+        }
+    res.send(results)
+    })
+})
+
+//delete user
+router.delete('/user', (req,res) => {
+    var id = req.body.id
+    sql = "DELETE FROM `user` WHERE `user`.`id` = " + id
+    db.query(sql, (err, results) => {
+        if(err){
+            throw err
+        }
+    res.send(results)
+    })
+})
+
+//update user by id
+router.patch('/user/:id', (req,res) => {
+    var newid = req.body.id
+    var id = req.body.id
+    var username = req.body.username
+    var password = req.body.password
+    var name = req.body.name
+    var email = req.body.email
+    var role = req.body.role
+    var departmentID = req.body.departmentID
+    var status = req.body.status
+    sql = "UPDATE `user` SET `id` = '" + newid + "', `username` = '" + username + "', `password` = '" + password + "', `name` = '" + name + "', `email` = '" + email + "', `role` = '" + role + "', `departmentID` = '" + departmentID + "', `status` = '" + status + "' WHERE `user`.`id` = " + id + " "
     db.query(sql, (err, results) => {
         if(err){
             throw err
