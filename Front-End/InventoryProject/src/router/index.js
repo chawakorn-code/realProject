764beb4257c1763231ca5ed2cb2caea97a6b8793
nekoimwza <57857355+nekoimwza@users.noreply.github.com>
2020-07-08 16:10:03 +0700

    Update index.js

--- Front-End/InventoryProject/src/router/index.js
+++ Front-End/InventoryProject/src/router/index.js
@@ -11,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path: '/profile',
-      name: 'Profile',
-      component: Profile
     }
   ]
 })
